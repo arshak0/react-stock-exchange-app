0281127accbf4a59c88eee743caf182dcd609c26
arshak0 <arshak.ishkhanyan@gmail.com>
2020-10-25 22:55:23 +0400

    app.js update + main.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './styles/main.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render () {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <hr>App for Stock Exchange</hr>
       </header>
+      <div>
+        <form>
+          <button>Start</button>
+        </form>
+        <form>
+          <button>Statistics</button>
+        </form>
+      </div>
     </div>
-  );
+  )
+  }
 }
 
 export default App;
