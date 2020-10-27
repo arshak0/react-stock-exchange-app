803a2ea679fa59cc855ba25bb71a4646468ee093
arshak0 <arshak.ishkhanyan@gmail.com>
2020-10-27 21:05:11 +0400

    changes 27-10

--- src/App.js
+++ src/App.js
@@ -14,15 +14,21 @@ class App extends React.Component {
       allValues: [],
       averageValue: null,
       standardDeviation: null,
-      moda: null,
+      modas: null,
+      modasValue: null,
       mediana: null,
-      showStatistics: false
+      showStatistics: false,
+      connected: false,
+      calculationTime: null
     }
   }
 
   //Clicking the "Start" button
   handleStartButton = e => {
     e.preventDefault();
+    this.setState({
+      connected: true
+    })
 
     this.handleSocket();
   };
@@ -71,6 +77,8 @@ class App extends React.Component {
 
   handleStatisticsShow = e => {
 
+    let timeFirst = (new Date()).getTime();
+
     this.setState({
       showStatistics: true
     })
@@ -93,32 +101,142 @@ class App extends React.Component {
       standardDeviation: Math.sqrt( forStandardDeviation/this.state.allValues.length )
     })
     
-    console.log(this.state.allValues)
+    //Finding out the moda
+    var objectDataset = {
+      most: [],
+      mostValue: 0
+    };
+  
+    this.state.allValues.forEach( current => {
+      if (!objectDataset[current]) {
+        objectDataset[current] = 0;
+      }
+  
+      objectDataset[current]++;
+      //
+      if ( objectDataset.most.length===0 ) {
+          objectDataset.most = [current];
+          objectDataset.mostValue = 1;
+      }
+      else if ( objectDataset[current] >= objectDataset[ objectDataset.most[0] ] ) {
+        if ( objectDataset[current] === objectDataset[ objectDataset.most[0] ] ) {
+          if ( !objectDataset.most.includes( current ) ) {
+            objectDataset.most.push( current );
+          }
+          objectDataset.mostValue = objectDataset[current];
+        }
+        else {
+          objectDataset.most=[current];
+          objectDataset.mostValue = objectDataset[current];
+        }
+      }
+      //
+    });
+
+    this.setState({
+      modas: objectDataset.most,
+      modasValue: objectDataset.mostValue
+    })
+    //-- End of Finding out the moda
+
+    //Finding out the mediana
+    let for_mediana=[];
+    this.state.allValues.forEach( (element, index) => {
+      for_mediana[index] = this.state.allValues[index]
+      
+    })
+
+    for_mediana.sort( (a, b) => {
+        return a - b;
+    });
+
+    if ( for_mediana.length%2!==0 ) {
+      this.setState({
+        mediana: for_mediana[ Math.floor( for_mediana.length/2 ) ]
+      })
+    }
+    else {
+      this.setState({
+        mediana: (   for_mediana[ for_mediana.length/2 ] + for_mediana[ for_mediana.length/2 - 1 ]  )/2
+      })
+    }
+    //-- End of Finding out the mediana
     
+    let timeSecond = (new Date()).getTime();
+
+    if ( (timeSecond - timeFirst)!==0 ) {
+      this.setState({
+        calculationTime: timeSecond - timeFirst
+      })
+    }
+    else {
+      this.setState({
+        calculationTime: 1
+      })
+    }   
+
+    this.consoleLogs( objectDataset, for_mediana );
+  }
+
+  consoleLogs = ( objectDataset, for_mediana ) => {
+    console.log( '-----------------' );
+    console.log( 'Сколько раз встречается мода/моды' );
+    console.log( objectDataset.mostValue );
+    console.log( 'Все значения' );
+    console.log( this.state.allValues );
+    console.log( 'Отсортированный массив для медианы' );
+    console.log( for_mediana );
+    console.log( '-----------------' );
   }
 
   render () {
+    const { modas } = this.state;
+
     return (
-      <div>
-        <h1>App for Stock Exchange</h1>
-        <div>
-          <form>
-            <button onClick={this.handleStartButton} >Start</button>
-          </form>
-          <form>
-            <button onClick={this.handleStatisticsButton} >Statistics</button>
-          </form>
+      <div className="app_body">
+
+        <div className="headline_block">
+          <img className="app_logo" src="/icon_logo.png" alt=""></img>
+          <h1 className="app_h1">App for Stock Exchange</h1>
+          <h2 className="app_h2"> По нажатию на “Старт” происходит подключение к эмулятору для получения котировок онлайн.
+            При нажатии на кнопку “Статистика” отображает на странице такие статистические значения: среднее, стандартное отклонение,
+            моду, медиану. Расчеты осуществляются по всем полученным данным от момента старта до текущего момента нажатия кнопки
+            “Статистика”, кнопку можно нажимать сколько угодно раз для получения новых результатов на текущее время.</h2>
+          <div  className="app_author_name_div">
+            <p className="app_author_name">Arshak Ishkhanyan </p>
+          </div>
         </div>
-        {this.state.showStatistics && <div className="statisticsBlock">
-          <p>Среднее арифметическое</p>
-            <p> { this.state.averageValue } </p>
-          <p>Стандартное отклонение</p>
-            <p> { this.state.standardDeviation } </p>
-          <p>Мода</p>
-            <p> { this.state.moda } </p>
-          <p>Медиана</p>
-            <p> { this.state.mediana } </p>
-        </div>}
+
+
+        <div className="app_main_body">
+          <div className="app_buttons_div">
+            <form className="app_button_form">
+              <button className="app_button" onClick={this.handleStartButton} >Start</button>
+            </form>
+            <form className="app_button_form">
+              <button className="app_button" onClick={this.handleStatisticsButton} >Statistics</button>
+            </form>
+            {this.state.connected && <div className="app_started">
+              <span className="app_connected_status app_connected_success"><i className="app_connected_pulse"></i>Connected</span>
+            </div>}
+          </div>
+          {this.state.showStatistics && <div className="app_statisticsBlock">
+            <p className="statistics_headline">Среднее арифметическое</p>
+              <p className="statistics_result"> { this.state.averageValue.toFixed(2) } </p>
+            <p className="statistics_headline">Стандартное отклонение</p>
+              <p className="statistics_result"> { this.state.standardDeviation.toFixed(2) } </p>
+            <p className="statistics_headline">Мода/Моды</p>
+              <div className="app_modas_div">
+                {modas.map( moda => <p className = "app_modas_div_p statistics_result" > {moda} </p> )}
+              </div>
+            <p className="statistics_headline">Сколько раз встречается мода/моды</p>
+              <p className="statistics_result"> { this.state.modasValue } </p>
+            <p className="statistics_headline mediana_headline">Медиана</p>
+              <p className="statistics_result"> { this.state.mediana } </p>
+            <p className="statistics_headline">Время вычислений (в миллисекундах)</p>
+              <p className="statistics_result"> { this.state.calculationTime } </p>
+          </div>}
+        </div>
       </div>
     )
   }
